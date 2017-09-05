c5cd3a652e93b12d0274301b170cd30b8c8934bb
kenda106 <31449719+kenda106@users.noreply.github.com>
2017-09-04 22:33:06 -0700

    Update mktmpfiles.sh
    Riley Kendall - Classwork 9/5

--- mktmpfiles.sh
+++ mktmpfiles.sh
@@ -5,3 +5,6 @@ do echo "" > "file_${i}.tmp";
 done;
 cd ..
 
+#I found it useful that I now understand how to commit files onto github and use bash scripts and their commands.
+#I still have questions on when to select certain file types in Cocalc, like when do I create a shell type file in the terminal rather 
+#than from the file selection drop-down bar?  

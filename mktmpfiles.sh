703d92ccbac6af59f5bdb1ac8c593f66718bb044
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 00:34:58 -0700

    Instructor note

--- mktmpfiles.sh
+++ mktmpfiles.sh
@@ -8,3 +8,13 @@ cd ..
 #I found it useful that I now understand how to commit files onto github and use bash scripts and their commands.
 #I still have questions on when to select certain file types in Cocalc, like when do I create a shell type file in the terminal rather 
 #than from the file selection drop-down bar?  
+
+###
+# INSTRUCTOR NOTE
+# 
+# Good. You can clean this up as follows:
+#  touch file{001..100}.tmp
+# This avoids the for loop entirely.
+#
+# Remember to edit the README file to add assessments, and names.
+###
